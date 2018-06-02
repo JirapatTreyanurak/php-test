876ab5b0796b64030aad2df70a5846b154260dcf
JirapatTreyanurak <j.treyanurak@gmail.com>
2018-06-02 16:01:24 +0100

    add a simple front-end

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+defined("TEMPLATES_PATH") or
+define("TEMPLATES_PATH", realpath(dirname(__FILE__)));
+
+$config = array(
+
+    "db" => array(
+        "dbname"    => "",
+        "username"  => "",
+        "password"  => "",
+        "host"      => ""
+    )
+
+);
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
-phpinfo();
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php-test/config.php";
 
+include_once TEMPLATES_PATH . "/header.php";
+include_once TEMPLATES_PATH . "/main.php";
+include_once TEMPLATES_PATH . "/footer.php";
+
+?>
